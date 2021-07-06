a9756bbb47ee81a51a863f43335d554893deb8db
SAURABH SHIRSATH <saurabhpshirsath@gmail.com>
2021-07-06 16:30:47 +0530

    UC-1_EmployeeAttendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,12 @@
-WELCOME
+#!/bin/bash -x
+
+echo "Welcome to Employee Wage Computation"
+
+Attendance=$((RANDOM%2))
+
+if [ $Attendance -eq 1 ]
+then
+    echo "Employee is Present"
+else
+    echo "Employee is Absent"
+fi
