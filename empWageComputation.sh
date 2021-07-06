e99a57735f61fbca704eb87a1f17cfeca1e595c6
SAURABH SHIRSATH <saurabhpshirsath@gmail.com>
2021-07-06 17:05:40 +0530

    UC-4_SwitchPartTimeEmpWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -10,14 +10,30 @@ fi
 
 isPartTime=0
 isFullTime=1
+isPartTime=1
+isFullTime=2
 empRatePerHr=20
 
- if [[ $isFullTime -eq $empCheck ]]
+empCheck=$((RANDOM%2))
+
+if [ $isFullTime -eq $empCheck ]
 then
     empHrs=8;
 else
-    empHrs=0;
     empHrs=4;
 fi
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+       $isFullTime)
+              empHrs=8;
+              ;;
+       $isPartTime)
+              empHrs=4;
+              ;;
+       *)
+              empHrs=0;
+              ;;
+esac
 
 dailyWage=$(($empHrs*$empRatePerHr))
