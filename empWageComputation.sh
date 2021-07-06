1c2be96acc68f64a15a1db54e9d22e917ad2bb8e
SAURABH SHIRSATH <saurabhpshirsath@gmail.com>
2021-07-06 17:51:15 +0530

    UC-7_GetWorkHours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,51 +7,37 @@ then
 else
     echo "Employee is Absent"
 fi
-
 isPartTime=1
 isFullTime=2
-monthlyWage=0
 empRatePerHr=20
 maxHrsInMonth=100
 numWorkingDays=20
-
-for (( day=1; day<=$numWorkingDays; day++ ))
-do
-
-empCheck=$((RANDOM%3))
-
-case $empCheck in
-       $isFullTime)
-              empHrs=8;
-              ;;
-       $isPartTime)
-              empHrs=4;
-              ;;
-       *)
-              empHrs=0;
-              ;;
-esac
 totalWorkHours=0
 totalWorkingDays=0
 
-dailyWage=$(($empHrs*$empRatePerHr))
-monthlyWage=$(($monthlyWage+$dailyWage))
 while [[ $totalWorkHours -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
 do
     ((totalWorkingDays++))
     empCheck=$((RANDOM%3))
          case $empCheck in
+Function GetWorkingHours () 
+{
+
+         case $1 in
                   $isFullTime)
                         empHrs=8;
                         ;;
-                  $isPartTime)
-                        empHrs=4;
-                        ;;
-                  *)
+@@ -35,7 +33,12 @@ do
                         empHrs=0;
                         ;;
          esac
          totalWorkHours=$(($totalWorkHours+$empHrs))
+}
+
+while [[ $totalWorkHours -lt $maxHrsInMonth && $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        getWorkingHours $((RANDOM%3))
+        totalWorkHours=$(($totalWorkHours+$empHrs))
 done
-done
-        echo monthlyWage=$(($totalWorkHours*$empRatePerHr))
+         monthlyWage=$(($totalWorkHours*$empRatePerHr))
